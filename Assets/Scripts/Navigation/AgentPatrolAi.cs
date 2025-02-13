d8265fa754d46d181ac7e0e38991d33c04db48d7
Daniel Thalmann <daniel@thalmann.li>
2025-02-13 22:15:16 +0100

    Ai with state

--- Assets/Scripts/Navigation/AgentPatrolAi.cs
+++ Assets/Scripts/Navigation/AgentPatrolAi.cs
@@ -1,4 +1,5 @@
 
+using System.Threading;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -9,19 +10,15 @@ public class AgentPatrolAi : MonoBehaviour
     protected Vector3 destination;
     protected float timeout;
 
-
-    protected bool inMoving;
     protected int index;
     protected AgentState state;
+    protected float refreshAgentPos;
 
     public Vector3 velocity { get; private set; }
 
     public float wait;
 
     public float limitDistance;
-
-    protected bool inWating;
-    protected bool inView;
 
     public GameObject[] patrolPoints;
 
@@ -31,106 +28,168 @@ public class AgentPatrolAi : MonoBehaviour
     {
         Wait,
         Patrol,
-        CatchUp,
+        PatrolFinish,
+        CatchIt,
+        CatchItFinish,
+        Attack
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        state = AgentState.Wait;
         index = 0;
         agent = GetComponent<NavMeshAgent>();
         destination = this.transform.position;
-        inMoving = false;
-        inWating = false;
-        inView = false;
         velocity = agent.velocity;
         timeout = 0;
-        state = AgentState.Wait;
+        refreshAgentPos = 0.50f;
 
     }
 
     public void Update()
     {
+        switch (state)
+        {
+            case AgentState.Wait:
+                Wait();
+                break;
+
+            case AgentState.PatrolFinish:
+                IsPatrolFinish();
+                break;
+
+            case AgentState.Patrol:
+                Patrol();
+                break;
+
+            case AgentState.CatchIt:
+                CatchIt();
+                break;
+
+            case AgentState.CatchItFinish:
+                IsCatchItFinish();
+                break;
+
+            case AgentState.Attack:
+                Attack();
+                break;
+        }
+
         if (patrolPoints.Length == 0)
         {
             return;
         }
         velocity = agent.velocity;
 
-        if (fov != null)
+    }
+
+    protected void Wait()
+    {
+        timeout += Time.deltaTime;
+        if (timeout > wait)
         {
-            if (fov.canSeePlayer)
-            {
-                inView = true;
-
-                Vector3 dir = Vector3.Normalize(fov.player.transform.position - transform.position);
-
-                Vector3 dest = fov.player.transform.position - (dir * limitDistance);
-                SetDestination(dest);
-
-            } else
-            {
-                if (inView)
-                {
-                    SetDestination(patrolPoints[index].transform.position);
-                    inView = false;
-                }
-            }
+            state = AgentState.Patrol;
         }
-
-        if (inMoving)
-        {
-            if (Vector3.Distance(transform.position, destination) < .1f)
-            {
-                inMoving = false;
-                inWating = true;
-                timeout = 0;
-            }
-        } 
         else
         {
-            if (inWating)
-            {
-                timeout += Time.deltaTime;
-                if (timeout > wait) 
-                {
-                    inWating = false;
-                    timeout = 0;
-                }
-
-            } else
-            {
-
-                SetDestination(patrolPoints[index].transform.position);
-                ++index;
-                if (index > patrolPoints.Length - 1)
-                    index = 0;
-            }
-
+            CanSeePlayer();
         }
 
     }
 
-    public void FixedUpdate()
+    protected bool CanSeePlayer()
     {
+        if (fov != null)
+        {
+            if (fov.canSeePlayer)
+            {
+                state = AgentState.CatchIt;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected void Patrol()
+    {
+        timeout = 0;
+        state = AgentState.PatrolFinish;
+        SetDestination(patrolPoints[index].transform.position);
+    }
+
+    protected void IsPatrolFinish()
+    {
+        timeout += Time.deltaTime;
+        if (timeout > refreshAgentPos)
+        {
+            timeout = 0;
+            CanSeePlayer();
+            return;
+        }
+
+        if (Vector3.Distance(transform.position, destination) < .1f)
+        {
+            timeout = 0;
+            state = AgentState.Wait;
+            ++index;
+            if (index > patrolPoints.Length - 1)
+                index = 0;
+        }
+        else
+        {
+            CanSeePlayer();
+        }
+    }
         
+    protected void CatchIt()
+    {
+        timeout = 0;
+        Vector3 diff = fov.player.transform.position - transform.position;
+        float length = diff.magnitude;
+        Vector3 dir = Vector3.Normalize(diff);
+        Vector3 dest = transform.position + (dir * (length - limitDistance));
+        SetDestination(dest);
+        state = AgentState.CatchItFinish;
+    }
+
+    protected void IsCatchItFinish()
+    {
+        timeout += Time.deltaTime;
+        if (timeout > refreshAgentPos)
+        {
+            timeout = 0;
+            CanSeePlayer();
+            return;
+        }
+        if (Vector3.Distance(transform.position, destination) < .1f)
+        {
+            timeout = 0;
+            state = AgentState.Attack;
+        }
+    }
+
+    protected void Attack()
+    {
+        timeout += Time.deltaTime;
+        if (timeout > 5.0f)
+        {
+            timeout = 0;
+            if (!CanSeePlayer())
+                state = AgentState.Wait;
+        } 
     }
 
     public void SetDestination(Vector3 destination)
     {
         this.destination = destination;
         agent.destination = destination;
-        inMoving = true;
-        timeout = 0;
         velocity = Vector3.zero;
     }
 
     private void OnDrawGizmos()
     {
-        if (inMoving)
-            Gizmos.color = Color.red;
-        else
-            Gizmos.color = Color.green;
+        Gizmos.color = Color.green;
 
         Gizmos.DrawSphere(destination, .2f);
 
